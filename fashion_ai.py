9413c6be513a6cee03221cdca815f144daf47c35
yueliang.cyl <yueliang.cyl@alibaba-inc.com>
2018-04-16 15:42:37 +0800

    update

--- fashion_ai.py
+++ fashion_ai.py
@@ -27,6 +27,7 @@ def plot_image(image_path):
 # plot_image(image_path[0][0])
 # print('label: ' + image_path[0][1])
 
+
 # 数据预处理与初始化
 def data_init(train_dir):
     mkdir_if_not_exist([train_dir, task])
@@ -50,13 +51,14 @@ def data_init(train_dir):
             shutil.copy(path, os.path.join(train_dir, task, 'val', str(label_index)))
         train_count += 1
      
-    
-    
-def get_model_net():
+
+def get_model_net(num_gpu):
     pretrained_net = models.resnet50_v2(pretrained=True)
     
-    num_gpu = 1
-    ctx = [mx.gpu(i) for i in range(num_gpu)] if num_gpu > 0 else [mx.cpu()]
+    if num_gpu > 0:
+        ctx = [mx.gpu(i) for i in range(num_gpu)] 
+    else:
+        ctx = [mx.cpu()]
     
     finetune_net = models.resnet50_v2(classes=6)
     finetune_net.features = pretrained_net.features
@@ -64,7 +66,6 @@ def get_model_net():
     finetune_net.collect_params().reset_ctx(ctx)
     finetune_net.hybridize()
     return finetune_net, ctx
-
 
 
 def calculate_ap(labels, outputs):
@@ -78,18 +79,20 @@ def calculate_ap(labels, outputs):
             cnt += 1
     return((ap, cnt))
 
+
 # 训练集图片增广（左右翻转，改颜色）
 def transform_train(data, label):
     im = data.astype('float32') / 255
-    auglist = image.CreateAugmenter(data_shape=(3, 224, 224), resize=256,
+    aug_list = image.CreateAugmenter(data_shape=(3, 224, 224), resize=512,
                                    rand_crop=True, rand_mirror=True,
-                                   mean = np.array([0.485, 0.456, 0.406]),
-                                   std = np.array([0.229, 0.224, 0.225]))
+                                   mean=np.array([0.485, 0.456, 0.406]),
+                                   std=np.array([0.229, 0.224, 0.225]))
     
-    for aug in auglist:
+    for aug in aug_list:
         im - aug(im)
-    im = nd.transpose(im, (2,0,1))
+    im = nd.transpose(im, (2, 0, 1))
     return (im, nd.array([label]).asscalar())
+
 
 # 验证集图片增广，没有随机裁剪和翻转
 def transform_val(data, label):
@@ -102,6 +105,7 @@ def transform_val(data, label):
         im - aug(im)
     im = nd.transpose(im, (2,0,1))
     return (im, nd.array([label]).asscalar())
+
 
 # 在验证集上预测并评估
 def validate(net, val_data, ctx):
@@ -122,7 +126,8 @@ def validate(net, val_data, ctx):
         AP_cnt += cnt
     _, val_acc = metric.get()
     return ((val_acc, AP / AP_cnt, val_loss / len(val_data)))
-    
+
+
 # =============================================================================
 lr = 1e-3
 momentum = 0.9
@@ -163,19 +168,26 @@ if __name__ == '__main__':
     val_path = os.path.join(train_data_dir, task, 'val')
     
     # 定义训练集的 DataLoader （分批读取）
+    # train_data = gluon.data.DataLoader(
+    #     gluon.data.vision.ImageFolderDataset(train_path, transform=transform_train),
+    #     batch_size=batch_size, shuffle=True, num_workers=1)
     train_data = gluon.data.DataLoader(
-        gluon.data.vision.ImageFolderDataset(train_path, transform=transform_train),
-        batch_size=batch_size, shuffle=True, num_workers=4)
-    
+        gluon.data.vision.ImageFolderDataset(train_path, transform=None),
+        batch_size=batch_size, shuffle=True, num_workers=1)
+
     # 定义验证集的 DataLoader
+    # val_data = gluon.data.DataLoader(
+    #     gluon.data.vision.ImageFolderDataset(val_path, transform=transform_val),
+    #     batch_size=batch_size, shuffle=False, num_workers=1)
     val_data = gluon.data.DataLoader(
-        gluon.data.vision.ImageFolderDataset(val_path, transform=transform_val),
-        batch_size=batch_size, shuffle=False, num_workers=4)
+        gluon.data.vision.ImageFolderDataset(val_path, transform=None),
+        batch_size=batch_size, shuffle=False, num_workers=1)
     
     # 获取迁移学习后的网络
     print("[get_model_net] start")
-    finetune_net, ctx = get_model_net()
+    finetune_net, ctx = get_model_net(1)
     print("[get_model_net] end")
+    print(ctx)
     
     trainer = gluon.Trainer(finetune_net.collect_params(),
                            'sgd', {'learning_rate': lr, 'momentum': momentum, 'wd': wd})
@@ -221,6 +233,5 @@ if __name__ == '__main__':
         train_loss /= num_batch
         
         val_acc, val_map, val_loss = validate(finetune_net, val_data, ctx)
-        print('[Epoch %d] Train-acc: %.3f, mAp: %.3f, loss: %.3f | val-acc: %.3f, mAP: %.3f, loss: %.3f | time: %.3f'%
+        print('[Epoch %d] Train-acc: %.3f, mAp: %.3f, loss: %.3f | val-acc: %.3f, mAP: %.3f, loss: %.3f | time: %.3f' %
              (epoch, train_acc, train_map, train_loss, val_acc, val_map, val_loss, time.time() - tic))
-    
